e9d5a8097366cabe1341530948aa84fbf646a98f
Chris Veness <chrisv@movable-type.co.uk>
2016-10-05 16:36:41 +0100

    Add squirrel command handling

--- main.js
+++ main.js
@@ -2,11 +2,15 @@
 /* main.js; adapted from electron.atom.io/docs/tutorial/quick-start with mods to run Koa.js app   */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
  
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron'); // www.npmjs.com/package/electron
+const spawn = require('child_process').spawn;       // nodejs.org/api/child_process.html
+const path = require('path');                       // nodejs.org/api/path.html
 
 let win = null; // keep global reference to window object to avoid automatic closing on JS GC
 
 function createWindow() {
+    if (handleSquirrelCommand()) return;
+
     console.log('createWindow');
     app.server = require('./app/app.js');                  // instantiate Koa app
     win = new BrowserWindow({ width: 1024, height: 768 }); // create browser window
@@ -23,3 +27,32 @@ app.on('window-all-closed', () => {               // quit when all windows are c
 app.on('activate', () => { // re-recreate window when dock icon is clicked and no other windows open
     if (win == null) createWindow();                     
 });
+
+
+// qv www.npmjs.com/package/electron-windows-installer
+function handleSquirrelCommand() {
+    if (process.platform != 'win32') return false; // only applies to Windows (win32 is both 32- & 64-bit)
+
+    const command = process.argv[1];
+    const target = path.basename(process.execPath);
+    
+    switch (command) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            update(['--createShortcut=' + target + ''], app.quit);
+            return true;
+        case '--squirrel-uninstall':
+            update(['--removeShortcut=' + target + ''], app.quit);
+            return true;
+        case '--squirrel-obsolete':
+            app.quit();
+            return true;
+    }
+
+    return false;
+}
+
+function update(args, done) {
+    const updateExe = path.resolve(path.dirname(process.execPath), '..', 'Update.exe');
+    spawn(updateExe, args, { detached: true }).on('close', done);
+}
